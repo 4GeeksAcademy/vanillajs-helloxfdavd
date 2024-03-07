0a0f957abc9b49ee065ed962734292898d3c9bf0
XefDavid <156122937+XefDavid@users.noreply.github.com>
2024-03-07 10:24:21 +0000

    myfirtcommit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  /* document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#escusa").innerHTML = generadorEscusas();
+  });
+  console.log("hello");
+};*/
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let pronombre in pronoun) {
+    for (let adjetivo in adj) {
+      for (let nombre in noun) {
+        console.log(pronoun[pronombre] + adj[adjetivo] + noun[nombre]);
+      }
+    }
+  }
 };
+console.log("Hello Rigo from the console!");
